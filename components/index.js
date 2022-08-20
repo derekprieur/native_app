0aa8167ec75e0304acf344e77d5d473a9cee7c5a
derekprieur <DPrieur123@gmail.com>
2022-08-20 13:54:06 -0400

    initial project

---
+++ components/index.js
@@ -0,0 +1,23 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import { NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo } from "./SubInfo";
+import DetailsDesc from './DetailsDesc'
+import DetailsBid from './DetailsBid'
+
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    CircleButton,
+    RectButton,
+    NFTTitle,
+    EthPrice,
+    ImageCmp,
+    People,
+    EndDate,
+    SubInfo,
+    DetailsDesc,
+    DetailsBid
+}
